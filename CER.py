d5e63e62762b1a7f190e05afaefc909d0125e5de
rjabowtschik <163625572+ryabov3@users.noreply.github.com>
2024-05-18 10:17:18 +0500

    Add files via upload

---
+++ CER.py
@@ -0,0 +1,22 @@
+import Levenshtein
+
+class Cer:
+
+    @staticmethod
+    def cer(predicted: str, target: str):
+        """
+        Вычислияет CER(Character Error Rate) между двумя строками.
+
+        :param predicted: str
+        :param target: str
+        :return: float
+        """
+
+        different = Levenshtein.distance(predicted, target)
+        return different / len(predicted)
+
+test1 = 'Arsenal is the best club in the world'
+test2 = 'ManCity is the best club in the world'
+
+dif = Cer.cer(test1, test2)
+print(dif)
